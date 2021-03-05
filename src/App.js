b728082929c278403d06730e704add88e602001b
shaikwasef <wasefshaik@gmail.com>
2021-03-05 19:06:10 +0530

    2 files modified

--- src/App.js
+++ src/App.js
@@ -1,15 +1,42 @@
 import React from "react";
 import "./style.css";
+import axios from "axios";
 
-class App extends React.Component(){
-  constructor(props){
+class App extends React.Component {
+  constructor(props) {
     super(props);
+    this.state = { characterName: [] };
   }
-  render(){
-  return (
-    <div></div>
-  );
+
+  async onEnter(event){
+    const book_url =
+      "https://www.anapioficeandfire.com/api/books/" + event.target.value;
+    var charactersUrls = await axios.get(book_url);
+    console.log(charactersUrls.data.characters.length);
+    var axiosRequests = [];
+    for(let i = 0 ; i< charactersUrls.data.characters.length ; i++){
+      axiosRequests.push(axios.get(charactersUrls.data.characters[i]));
+    } 
+    axios.all(axiosRequests).then(axios.spread((...responses) => {
+      for(let i =0 ; i < responses.length ; i++){
+        this.setState({characterName : this.state.characterName.concat(responses[i].data.name)});
+      }})).catch((error) => console.log(error));
+    }
+
+  render() {
+    const names = this.state.characterName.map((name ,i) => {
+      return <li key = {i}>{name}jhjb</li>;}
+    );
+    return (
+      <div>
+        <input
+          onKeyPress={() => this.onEnter(event)}
+          placeholder="Enter Book Number"
+        />
+        {names}
+      </div>
+    );
   }
 }
 
-export default App ;
+export default App;
